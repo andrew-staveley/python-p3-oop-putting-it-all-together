1b8da2723acfea08107369370d2a86215b8e1418
Andrew <andrewstaveley14@gmail.com>
2024-01-27 19:09:56 -0500

    Completed Assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size if (type(size) is (int)) else print("size must be an integer")
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
